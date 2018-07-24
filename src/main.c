4868ac3ebe5f586d2874b2d02a6eae01265cb69e
Felix Guo <felixguoxiuping@gmail.com>
2018-07-23 22:50:59 -0700

    Added help

--- src/main.c
+++ src/main.c
@@ -57,14 +57,19 @@ void invalid_usage(void) {
 	printf("    --ast             : prints out the constructed AST.\n");
 	printf("    -t, --token-list  : prints out the parsed tokens.\n");
 	printf("    -d --disassemble  : prints out the disassembled bytecode.\n");
-	printf("	--dependencies    : prints out the module dependencies of the file.\n");
+	printf("    --dependencies    : prints out the module dependencies of the file.\n");
 	printf("    --sandbox         : runs the VM in sandboxed mode, ie. no file access and no native execution calls.\n");
 	printf("\nWendy will enter REPL mode if no parameters are supplied.\n");
 	safe_exit(1);
 }
 
-void process_options(char** options, int len) {
+// The first non-valid option is typically the file name / source string.
+// The other non-valid options are the arguments.
+// Returns true if user prompted for help.
+bool process_options(char** options, int len, char** source) {
 	int i;
+	bool has_encountered_invalid = false;
+	*source = NULL;
 	for (i = 0; i < len; i++) {
 		if (streq("-c", options[i]) ||
 			streq("--compile", options[i])) {
@@ -97,6 +102,18 @@ void process_options(char** options, int len) {
 				 streq("--disassemble", options[i])) {
 			set_settings_flag(SETTINGS_DISASSEMBLE);
 		}
+		else if (streq("-d", options[i]) ||
+				 streq("--disassemble", options[i])) {
+			set_settings_flag(SETTINGS_DISASSEMBLE);
+		}
+		else if (streq("-h", options[i]) ||
+				 streq("--help", options[i])) {
+			return true;
+		}
+		else if (!has_encountered_invalid) {
+			*source = options[i];
+			has_encountered_invalid = true;
+		}
 		else {
 			// Once we hit a not-valid option, the rest are all arguments to
 			//   program.
@@ -105,6 +122,7 @@ void process_options(char** options, int len) {
 	}
 	program_arguments_count = len - i;
 	program_arguments = &options[i];
+	return false;
 }
 
 void run(char* input_string) {
@@ -156,195 +174,196 @@ bool bracket_check(char* source) {
 	return !p && !s && !c && !in_string;
 }
 
+int repl(void) {
+	init_source(0, "", 0, false);
+	printf("Welcome to " WENDY_VERSION " created by: Felix Guo\n");
+	printf(BUILD_VERSION "\n");
+	printf("Run `wendy -help` to get help.\n");
+	printf("Press Ctrl+D (EOF) to exit REPL.\n");
+	char* path = get_path();
+	printf("Path: %s\n", path);
+	printf("Note: REPL does not show call stack on error, run with -v to dump call stacks.\n");
+	safe_free(path);
+	char* input_buffer;
+	char* source_to_run = safe_malloc(1 * sizeof(char));
+	// ENTER REPL MODE
+	set_settings_flag(SETTINGS_REPL);
+	push_frame("main", 0, 0);
+	bool has_run = false;
+	forever {
+		size_t source_size = 1;
+		source_to_run[0] = 0;
+		bool first = true;
+		// Perform bracket check to determine whether or not to execute:
+		while (!bracket_check(source_to_run)) {
+			if (first) {
+				input_buffer = readline("> ");
+				first = false;
+			}
+			else {
+				input_buffer = readline("  ");
+			}
+			if(!input_buffer) {
+				printf("\n");
+				goto cleanup;
+			}
+			source_size += strlen(input_buffer);
+			source_to_run = safe_realloc(source_to_run,
+				source_size * sizeof(char));
+			strcat(source_to_run, input_buffer);
+			free(input_buffer);
+		}
+		add_history(source_to_run);
+		run(source_to_run);
+		has_run = true;
+		unwind_stack();
+	}
+	safe_free(source_to_run);
+
+cleanup:
+	c_free_memory();
+	if (has_run) {
+		vm_cleanup_if_repl();
+	}
+	return 0;
+}
+
 int main(int argc, char** argv) {
 	init_memory();
 	determine_endianness();
-	if (argc == 1) {
-		init_source(0, "", 0, false);
-		printf("Welcome to " WENDY_VERSION " created by: Felix Guo\n");
-		printf(BUILD_VERSION "\n");
-		printf("Run `wendy -help` to get help.\n");
-		printf("Press Ctrl+D (EOF) to exit REPL.\n");
-		char* path = get_path();
-		printf("Path: %s\n", path);
-		printf("Note: REPL does not show call stack on error, run with -v to dump call stacks.\n");
-		safe_free(path);
-		char* input_buffer;
-		char* source_to_run = safe_malloc(1 * sizeof(char));
-		// ENTER REPL MODE
-		set_settings_flag(SETTINGS_REPL);
-		push_frame("main", 0, 0);
-		bool has_run = false;
-		forever {
-			size_t source_size = 1;
-			source_to_run[0] = 0;
-			bool first = true;
-			// Perform bracket check to determine whether or not to execute:
-			while (!bracket_check(source_to_run)) {
-				if (first) {
-					input_buffer = readline("> ");
-					first = false;
-				}
-				else {
-					input_buffer = readline("  ");
-				}
-				if(!input_buffer) {
-					printf("\n");
-					goto cleanup;
-				}
-				source_size += strlen(input_buffer);
-				source_to_run = safe_realloc(source_to_run,
-					source_size * sizeof(char));
-				strcat(source_to_run, input_buffer);
-				free(input_buffer);
-			}
-			add_history(source_to_run);
-			run(source_to_run);
-			has_run = true;
-			unwind_stack();
-		}
-		safe_free(source_to_run);
-
-	cleanup:
-		c_free_memory();
-		if (has_run) {
-			vm_cleanup_if_repl();
-		}
-		return 0;
-	}
-	set_settings_flag(SETTINGS_STRICT_ERROR);
-	if (argc != 2) {
-		process_options(&argv[2], argc - 2);
-	}
-	if (argc == 2 && (streq(argv[1], "--help") ||
-					  streq(argv[1], "-h"))) {
+	char *option_result;
+	if (process_options(&argv[1], argc - 1, &option_result)) {
+		// User asked for -h / --help
 		invalid_usage();
 	}
-	else if (argc >= 2) {
-		// FILE READ MODE
-		long length = 0;
-		int file_name_length = strlen(argv[1]);
-		FILE* file = fopen(argv[1], "r");
-		if (!file) {
-			// Attempt to run as source string
-			push_frame("main", 0, 0);
-			run(argv[1]);
-			goto wendy_exit;
+	if (!option_result) {
+		return repl();
+	}
+	set_settings_flag(SETTINGS_STRICT_ERROR);
+	// FILE READ MODE
+	long length = 0;
+	int file_name_length = strlen(option_result);
+	FILE* file = fopen(option_result, "r");
+	if (!file) {
+		// Attempt to run as source string
+		push_frame("main", 0, 0);
+		run(option_result);
+		goto wendy_exit;
+	}
+	// Compute File Size
+	fseek(file, 0, SEEK_END);
+	length = ftell(file);
+	fseek (file, 0, SEEK_SET);
+	bool is_compiled = false;
+	int header_length = strlen(WENDY_VM_HEADER);
+	if (length > header_length) {
+		char header[header_length + 1];
+		fread(header, sizeof(char), header_length + 1, file);
+		fseek(file, 0, SEEK_SET);
+		if (streq(WENDY_VM_HEADER, header)) {
+			is_compiled = true;
 		}
-		// Compute File Size
-		fseek(file, 0, SEEK_END);
-		length = ftell(file);
-		fseek (file, 0, SEEK_SET);
-		bool is_compiled = false;
-		int header_length = strlen(WENDY_VM_HEADER);
-		if (length > header_length) {
-			char header[header_length + 1];
-			fread(header, sizeof(char), header_length + 1, file);
-			fseek(file, 0, SEEK_SET);
-			if (streq(WENDY_VM_HEADER, header)) {
-				is_compiled = true;
-			}
+	}
+	// File Pointer should be reset
+	uint8_t* bytecode_stream;
+	size_t size;
+	if (!is_compiled) {
+		init_source(file, option_result, length, true);
+		// Text Source
+		char* buffer = get_source_buffer();
+		// Begin Processing the File
+		size_t alloc_size = 0;
+		token* tokens;
+		size_t tokens_count;
+
+		// Scanning and Tokenizing
+		tokens_count = scan_tokens(buffer, &tokens, &alloc_size);
+		if (get_settings_flag(SETTINGS_TOKEN_LIST_PRINT)) {
+			print_token_list(tokens, tokens_count);
 		}
-		// File Pointer should be reset
-		uint8_t* bytecode_stream;
-		size_t size;
-		if (!is_compiled) {
-			init_source(file, argv[1], length, true);
-			// Text Source
-			char* buffer = get_source_buffer();
-			// Begin Processing the File
-			size_t alloc_size = 0;
-			token* tokens;
-			size_t tokens_count;
 
-			// Scanning and Tokenizing
-			tokens_count = scan_tokens(buffer, &tokens, &alloc_size);
-			if (get_settings_flag(SETTINGS_TOKEN_LIST_PRINT)) {
-				print_token_list(tokens, tokens_count);
-			}
+		// Build AST
+		statement_list* ast = generate_ast(tokens, tokens_count);
+		if (get_settings_flag(SETTINGS_OPTIMIZE)) {
+			ast = optimize_ast(ast);
+		}
+		if (get_settings_flag(SETTINGS_ASTPRINT)) {
+			print_ast(ast);
+		}
 
-			// Build AST
-			statement_list* ast = generate_ast(tokens, tokens_count);
-			if (get_settings_flag(SETTINGS_OPTIMIZE)) {
-				ast = optimize_ast(ast);
-			}
-			if (get_settings_flag(SETTINGS_ASTPRINT)) {
-				print_ast(ast);
-			}
-
-			if (get_settings_flag(SETTINGS_OUTPUT_DEPENDENCIES)) {
-				// Perform static analysis to output dependencies
-				print_dependencies(ast);
-				safe_free(tokens);
-				free_ast(ast);
-				goto wendy_exit;
-			}
-			else {
-				// Generate Bytecode
-				bytecode_stream = generate_code(ast, &size);
-			}
+		if (get_settings_flag(SETTINGS_OUTPUT_DEPENDENCIES)) {
+			// Perform static analysis to output dependencies
+			print_dependencies(ast);
 			safe_free(tokens);
 			free_ast(ast);
+			goto wendy_exit;
 		}
 		else {
-			// Compiled Source
-			char* search_name = safe_malloc(sizeof(char) *
-					(strlen(argv[1]) + 1));
-			strcpy(search_name, argv[1]);
-			int i = strlen(search_name);
-			while (search_name[i] != '.') i--;
-			search_name[i + 1] = 0;
-			strcat(search_name, "w");
-			FILE* source_file = fopen(search_name, "r");
-			if (source_file) {
-				fseek(source_file, 0, SEEK_END);
-				long length = ftell(source_file);
-				fseek (source_file, 0, SEEK_SET);
-				init_source(source_file, search_name, length, false);
-				fclose(source_file);
-			}
-			else {
-				init_source(0, "", 0, false);
-			}
-			safe_free(search_name);
-			bytecode_stream = safe_malloc(sizeof(uint8_t) * length);
-			fread(bytecode_stream, sizeof(uint8_t), length, file);
+			// Generate Bytecode
+			bytecode_stream = generate_code(ast, &size);
 		}
-		fclose(file);
-		if (get_settings_flag(SETTINGS_DISASSEMBLE)) {
-			print_bytecode(bytecode_stream, stdout);
-		}
-		if (get_settings_flag(SETTINGS_COMPILE)) {
-			if (is_compiled) {
-				printf("Compile flag set, but input file was already compiled.\n");
-			}
-			else {
-				int new_file_length = file_name_length + 2;
-				char* compile_path = safe_malloc(new_file_length * sizeof(char));
-				compile_path[0] = 0;
-				strcpy(compile_path, argv[1]);
-				strcat(compile_path, "c");
-
-				FILE* compile_file = fopen(compile_path, "w");
-				if (compile_file) {
-					write_bytecode(bytecode_stream, compile_file);
-					printf("Successfully compiled into %s.\n", compile_path);
-				}
-				else {
-					printf("Error opening compile file to write.\n");
-				}
-				if (compile_path) safe_free(compile_path);
-			}
-		}
-		else {
-			push_frame("main", 0, 0);
-			vm_run(bytecode_stream, size);
-			if (!last_printed_newline) {
-				printf("\n");
-			}
-		}
-		safe_free(bytecode_stream);
+		safe_free(tokens);
+		free_ast(ast);
 	}
+	else {
+		// Compiled Source
+		char* search_name = safe_malloc(sizeof(char) *
+				(strlen(option_result) + 1));
+		strcpy(search_name, option_result);
+		int i = strlen(search_name);
+		while (search_name[i] != '.') i--;
+		search_name[i + 1] = 0;
+		strcat(search_name, "w");
+		FILE* source_file = fopen(search_name, "r");
+		if (source_file) {
+			fseek(source_file, 0, SEEK_END);
+			long length = ftell(source_file);
+			fseek (source_file, 0, SEEK_SET);
+			init_source(source_file, search_name, length, false);
+			fclose(source_file);
+		}
+		else {
+			init_source(0, "", 0, false);
+		}
+		safe_free(search_name);
+		bytecode_stream = safe_malloc(sizeof(uint8_t) * length);
+		fread(bytecode_stream, sizeof(uint8_t), length, file);
+	}
+	fclose(file);
+	if (get_settings_flag(SETTINGS_DISASSEMBLE)) {
+		print_bytecode(bytecode_stream, stdout);
+	}
+	if (get_settings_flag(SETTINGS_COMPILE)) {
+		if (is_compiled) {
+			printf("Compile flag set, but input file was already compiled.\n");
+		}
+		else {
+			int new_file_length = file_name_length + 2;
+			char* compile_path = safe_malloc(new_file_length * sizeof(char));
+			compile_path[0] = 0;
+			strcpy(compile_path, option_result);
+			strcat(compile_path, "c");
+
+			FILE* compile_file = fopen(compile_path, "w");
+			if (compile_file) {
+				write_bytecode(bytecode_stream, compile_file);
+				printf("Successfully compiled into %s.\n", compile_path);
+			}
+			else {
+				printf("Error opening compile file to write.\n");
+			}
+			if (compile_path) safe_free(compile_path);
+		}
+	}
+	else {
+		push_frame("main", 0, 0);
+		vm_run(bytecode_stream, size);
+		if (!last_printed_newline) {
+			printf("\n");
+		}
+	}
+	safe_free(bytecode_stream);
+
 wendy_exit:
 	free_source();
 	c_free_memory();
